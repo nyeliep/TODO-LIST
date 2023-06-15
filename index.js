74c8aa794266c4360809dc6b6e7b48d165cf31e4
nyeliep <gielpalnyeliep@gmail.com>
2023-06-15 09:31:38 +0300

    commit

--- index.js
+++ index.js
@@ -1,76 +1,63 @@
-const todoContainer = document.getElementById('list');
-const input = document.getElementById('input');
-let todos = [];
+const myTodo = document.getElementById('myUL');
 
-const newElement = () => {
-  const task = input.value;
-  if (task.trim() !== '') {
-    const todo = {
-      id: Date.now(),
-      task,
-      completed: false
-    };
-    todos.push(todo);
-    displayTodos();
-    input.value = '';
-  }
-};
-
-const handleCompletionChange = (event, id) => {
-  const todo = todos.find(item => item.id === id);
-  if (todo) {
-    todo.completed = event.target.checked;
-    displayTodos();
-  }
-};
-
-const deleteTodo = (id) => {
-  todos = todos.filter(item => item.id !== id);
-  displayTodos();
-};
-
-const displayTodos = () => {
-  todoContainer.innerHTML = '';
-  todos.forEach(item => {
-    const li = document.createElement('li');
-    const todo = document.createElement('span');
-    const deleteButton = document.createElement('button');
-    todo.textContent = item.task;
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', () => deleteTodo(item.id));
-    li.appendChild(todo);
-    li.appendChild(deleteButton);
-    if (item.completed) {
-      li.classList.add('completed');
+const fetchTodos = async () => {
+  try {
+    const response = await fetch('https://dummyjson.com/todos');
+    if (!response.ok) {
+      throw new Error('Error fetching todos');
     }
-    todoContainer.appendChild(li);
-  });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching todos:", error);
+    throw error;
+  }
 };
 
+const displayTodos = async () => {
+  try {
+    const todosResponse = await fetchTodos();
+    console.log(JSON.stringify(todosResponse));
 
+    if (Array.isArray(todosResponse.todos)) {
+      const todos = todosResponse.todos;
+      todos.forEach((todo, index) => {
+        const listItem = createTodoItem(todo, index);
+        myTodo.appendChild(listItem);
+      });
+    } else {
+      console.error("Error displaying todos: Invalid response structure");
+    }
+  } catch (error) {
+    console.error("Error displaying todos:", error);
+  }
+};
 
+const createTodoItem = (todo, index) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = `${index}: ${JSON.stringify(todo)}`;
 
-// other js
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove';
+  removeButton.classList.add('remove-button')
+  removeButton.addEventListener('click', () => {
+    listItem.remove();
+  });
 
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+  listItem.appendChild(removeButton);
+
+  return listItem;
+};
+
+displayTodos();
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+for (var j = 0; j < close.length; j++) {
+  close[j].addEventListener("click", function() {
     var div = this.parentElement;
-    div.style.display = "none";
-  }
+    div.remove();
+  });
 }
 
 // Add a "checked" symbol when clicking on a list item
@@ -83,27 +70,18 @@ list.addEventListener('click', function(ev) {
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
+  var li = createTodoItem(document.getElementById("myInput").value, 0);
+  if (li.textContent === '') {
     alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  var close = li.getElementsByClassName("close");
+  close[0].addEventListener("click", function() {
+    var div = this.parentElement;
+    div.remove();
+  });
 }
+
